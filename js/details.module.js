e55ff3da43895342edd9702cf7798d886046337d
Mahmoud Younis <mahmoudyounis5761@gmail.com>
2024-08-17 03:33:31 +0300

    Add files via upload

---
+++ js/details.module.js
@@ -0,0 +1,36 @@
+import { Ui } from "./ui.module.js";
+
+export class Details {
+  constructor(id) {
+    this.ui = new Ui();
+
+    document.getElementById("btnClose").addEventListener("click", () => {
+      document.querySelector(".games").classList.remove("d-none");
+      document.querySelector(".details").classList.add("d-none");
+    });
+
+    this.getDetails(id);
+  }
+  getDetails(idGames) {
+    const loading = document.querySelector(".loading-screen");
+    $(".loading-screen").css('z-index','9999999999999999999999999').css('display','block')
+    $("body").css("overflow", "auto");
+
+    
+    const options = {
+       method: "GET",
+       headers: {
+          "X-RapidAPI-Key": "761b8a3226msh868f0d927cb6ea4p117ef0jsn46d63d281712",
+          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+       },
+    };
+
+    fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${idGames}`, options)
+       .then((response) => response.json())
+       .then((response) => this.ui.displayDetails(response))
+       .catch((err) => console.error(err))
+       .finally(() => {
+        $(".loading-screen").css('z-index','0').css('display','none')
+       });
+ }
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { Games } from "./games.module.js";
+new Games();
+
+$(document).ready(function () {
+  $(".loading-screen").fadeOut(1500);
+  $("body").css("overflow", "auto");
+});
